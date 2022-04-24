51aadd59038154a3f3ae01e9acf557d489320bba
Pradhyumna-coding <kiranvittalrao1@gmail.com>
2022-04-24 17:14:04 +0530

    Class - 1:Output

---
+++ Class 1 - Output/details.py
@@ -0,0 +1,9 @@
+name = "Pradhyumna"
+standard = 9
+school = "Carmel"
+city = "Bengaluru"
+
+print("My name is",name)
+print("\nMy standard is",standard)
+print("I study in",school,"school")
+print("I live in",city)
