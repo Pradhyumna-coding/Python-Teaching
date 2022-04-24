fe785fe746705e3cfdea1cada70e5ab45ef5159e
Pradhyumna-coding <kiranvittalrao1@gmail.com>
2022-04-24 19:12:03 +0530

    Class - 2 : Input

---
+++ Class - 2 - Input/main.py
@@ -0,0 +1,7 @@
+name = input("What is your name?:")
+date = input("When is your birthday?:")
+city = input("Where do you live?:")
+
+print("\nYour name is",name)
+print("Your birthday is on",date)
+print("Your live in",city)
