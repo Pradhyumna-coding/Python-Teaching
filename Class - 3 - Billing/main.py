eb20f28833d02ceaddee2cd62c85a32f93ca0e3d
Pradhyumna-coding <kiranvittalrao1@gmail.com>
2022-04-26 18:05:28 +0530

    Class - 3 files

---
+++ Class - 3 - Billing/main.py
@@ -0,0 +1,17 @@
+burgerCost = 45
+pizzaCost = 90
+iceCost = 20
+
+noPerson = int(input("How many people are there:"))
+noBurger = int(input("No. of burgers:"))
+noPizza = int(input("No. of pizzas:"))
+noIce = int(input("No. of Ice Creams:"))
+
+burgerTotal = noBurger*burgerCost
+pizzaTotal = noPizza*pizzaCost
+iceTotal = noIce*iceCost
+
+total = burgerTotal+pizzaTotal+iceTotal
+print("The total is "+str(total))
+average = total/noPerson
+print("Each person has to pay "+str(average))
