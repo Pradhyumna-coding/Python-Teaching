e6a137ff71081170ecbebc296cdfb36409f3057e
Pradhyumna-coding <kiranvittalrao1@gmail.com>
2022-04-26 16:59:51 +0530

    Homework for Class-2 added

---
+++ Class - 2 - Input/homework.py
@@ -0,0 +1,6 @@
+streetName  = input("What is the name of your street?:")
+city = input("What is the name of your city")
+state = input("What is the name of your state")
+country = input("What is the name of your country")
+
+print("Your address is",streetName,",",city,",",state,",",country)
